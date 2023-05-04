213aa4d2ef8be3541cca3788b32c8cdf265e0fe4
Sudarshan Pathak <sushan.pth@gmail.com>
2023-05-04 13:51:06 +0545

    insert a new order function

--- internal/models/models.go
+++ internal/models/models.go
@@ -138,3 +138,31 @@ func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
 	return int(id), nil
 
 }
+
+// InsertOrder inserts a order into the database and returns the id
+func (m *DBModel) InsertOrder(order Order) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+			insert into orders  (
+				widget_id, transaction_id, status_id, quantity, 
+				amount, created_at, updated_at
+			)
+			values (?, ?, ?, ?, ?, ?, ?)
+			`
+	result, err := m.DB.ExecContext(ctx, stmt,
+		order.WidgetID, order.TransactionID, order.StatusID, order.Quantity,
+		order.Amount, time.Now(), time.Now(),
+	)
+
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
+
+}
