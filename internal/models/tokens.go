7b83b0db2e90d93e5b5c462cdb711171ab3e60b1
Sudarshan Pathak <sushan.pth@gmail.com>
2023-05-10 10:31:04 +0545

    save token to localstorage

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -44,10 +44,18 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `insert into tokens (user_id, name, email, token_hash, created_at, updated_at) 
+	// delete existing tokens
+	stmt := `delete from tokens where user_id = ?`
+	_, err := m.DB.ExecContext(ctx, stmt, u.ID)
+
+	if err != nil {
+		return err
+	}
+
+	stmt = `insert into tokens (user_id, name, email, token_hash, created_at, updated_at) 
 					values (?, ?, ?, ?, ?, ?)`
 
-	_, err := m.DB.ExecContext(ctx, stmt,
+	_, err = m.DB.ExecContext(ctx, stmt,
 		u.ID,
 		u.LastName,
 		u.Email,
